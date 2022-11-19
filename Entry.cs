2569bb3e1a5302284a94f39021bef53ec532fb7c
vman <public@vaughnnugent.com>
2022-11-19 01:18:21 -0500

    License updates

--- Entry.cs
+++ Entry.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿/*
+* Copyright (c) 2022 Vaughn Nugent
+* 
+* Library: VNLib
+* Package: VNLib.WebServer
+* File: Entry.cs 
+*
+* Entry.cs is part of VNLib.WebServer which is part of the larger 
+* VNLib collection of libraries and utilities.
+*
+* VNLib.WebServer is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU General Public License as published
+* by the Free Software Foundation, either version 2 of the License,
+* or (at your option) any later version.
+*
+* VNLib.WebServer is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License 
+* along with VNLib.WebServer. If not, see http://www.gnu.org/licenses/.
+*/
+
+using System;
 using System.IO;
 using System.Net;
 using System.Data;
@@ -19,13 +43,12 @@ using VNLib.Utils.Memory;
 using VNLib.Utils.Logging;
 using VNLib.Utils.Extensions;
 using VNLib.Net.Http;
-using VNLib.Net.Transport.Tcp;
 using VNLib.Plugins.Essentials.ServiceStack;
-using HttpVersion = VNLib.Net.Http.HttpVersion;
 
 using VNLib.WebServer.Transport;
 using VNLib.WebServer.TcpMemoryPool;
 using VNLib.WebServer.RuntimeLoading;
+using VNLib.Net.Transport.Tcp;
 
 /*
 * Arguments
@@ -462,7 +485,7 @@ Starting...
 
                     HttpEncoding = Encoding.ASCII,
                 };
-                return conf.DefaultHttpVersion == HttpVersion.NotSupported
+                return conf.DefaultHttpVersion == Net.Http.HttpVersion.NotSupported
                     ? throw new Exception("default_version is invalid, specify an RFC formatted http version 'HTTP/x.x'")
                     : conf;
             }
--- VirtualHost.cs
+++ VirtualHost.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿/*
+* Copyright (c) 2022 Vaughn Nugent
+* 
+* Library: VNLib
+* Package: VNLib.WebServer
+* File: VirtualHost.cs 
+*
+* VirtualHost.cs is part of VNLib.WebServer which is part of the larger 
+* VNLib collection of libraries and utilities.
+*
+* VNLib.WebServer is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU General Public License as published
+* by the Free Software Foundation, either version 2 of the License,
+* or (at your option) any later version.
+*
+* VNLib.WebServer is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License 
+* along with VNLib.WebServer. If not, see http://www.gnu.org/licenses/.
+*/
+
+using System;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using System.Security.Authentication;
-
-using VNLib.Net;
 using VNLib.Net.Http;
 using VNLib.Utils.Memory;
 using VNLib.Utils.Logging;
